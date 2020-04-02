24823137b841a47bb8ffab960bac52c2a20717ed
RAJ836 <raj.annam5@gmail.com>
2020-04-02 23:07:10 +0530

    numerictypes

---
+++ Basics/hello.py
@@ -0,0 +1,4 @@
+'''
+created by Raj on 04/02/2020
+'''
+print("Hello world")
---
+++ Basics/numerictypes.py
@@ -0,0 +1,18 @@
+a=13
+b=100
+c=-66
+print(f'integers : {a,b,c}')
+print(a,b,c)
+print(f'type of a : {type(a)}')
+d=3+4j
+print(f'value of d : {d} and it belongs to {type(d)}')
+
+e=0b1010
+print(f'value of e : {e} and type : {type(e)}')
+
+f= True
+print(f'value of f : {f} and type : {type(f)}')
+
+print(f'binary form of 10 : {bin(10)}')
+print(f'hexadeicmal form of 10 : {hex(10)}')
+print(f'octal form of 10 : {oct(10)}')
